0ec324aa96074a5accf44024570317e726b0e960
dod401 <domdanesi@gmail.com>
2026-05-09 00:00:02 -0400

    First commit, WIP authentication service.

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+const users = [];
+
+app.get('/users', (req, res) => {
+  res.json(users);
+});
+
+app.post('/users', (req, res) => {
+  const { name, password } = req.body;
+  const newUser = { id: users.length + 1, name, password };
+  users.push(newUser);
+  res.status(201).json(newUser);
+});
+
+app.listen(3000, () => {
+  console.log(`Server is running on port ${3000}`);
+});
