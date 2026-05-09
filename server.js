d0fca9d9f4dbddd23530a9b94bfbf4149415813b
dod401 <domdanesi@gmail.com>
2026-05-09 11:58:07 -0400

    Fixed tsconfig.json to use commonjs module system and node module resolution, and added outDir and rootDir options for better project structure. Also added ignoreDeprecations to suppress warnings about deprecated features in TypeScript 6.0.

--- server.js
+++ server.js
@@ -1,19 +1,18 @@
-const express = require('express');
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require("express");
 const app = express();
 const PORT = 3000;
 const users = [];
-
 app.get('/users', (req, res) => {
-  res.json(users);
+    res.json(users);
 });
-
 app.post('/users', (req, res) => {
-  const { name, password } = req.body;
-  const newUser = { id: users.length + 1, name, password };
-  users.push(newUser);
-  res.status(201).json(newUser);
+    const { name, password } = req.body;
+    const newUser = { id: users.length + 1, name, password };
+    users.push(newUser);
+    res.status(201).json(newUser);
 });
-
 app.listen(3000, () => {
-  console.log(`Server is running on port ${3000}`);
+    console.log(`Server is running on port ${3000}`);
 });
---
+++ server.ts
@@ -0,0 +1,20 @@
+import express = require("express");
+import { Request, Response } from "express";
+const app = express();
+const PORT = 3000;
+const users: { id: number; name: string; password: string }[] = [];
+
+app.get('/users', (req, res) => {
+  res.json(users);
+});
+
+app.post('/users', (req, res) => {
+  const { name, password } = req.body;
+  const newUser = { id: users.length + 1, name, password };
+  users.push(newUser);
+  res.status(201).json(newUser);
+});
+
+app.listen(3000, () => {
+  console.log(`Server is running on port ${3000}`);
+});
